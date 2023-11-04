edb0588bdbba2f904b5313d77a44ef34e48eb9ef
Bobokhonov Aminjon <aminjohn16sss@gmail.com>
2023-11-04 13:43:38 +0300

    Product and Recipe models

---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodServingCost
+{
+    internal class Product
+    {
+        public string Name { get; set; }    //Название продукта
+        public double Volume { get; set; }  //Объём (количество)
+        public double Price { get; set; }   //Стоимость
+    }
+}
---
+++ Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodServingCost
+{
+    internal class Recipe
+    {
+        public string Name { get; set; }    //Название рецепта
+        public List<Product> products { get; set; } //Лист продуктов необх. для рецепта
+        public List<double> required_volume { get; set; }   //Лист необх-го объёма каждого продукта
+        public int servings { get; set; }   // Кол-во порций
+    }
+}
